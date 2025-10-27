4c4b810d1f2ba921420c08732e775cdb990164a4
휘래 김 <hramst0618@gmail.com>
2025-10-27 23:29:58 +0900

    feat: cors 정책 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRouter from './routes/auth.router.js';
 import { errorHandler } from './middlewares/error-handler.middleware.js';
@@ -9,6 +10,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
 
 app.use('/api/auth', authRouter);
 
