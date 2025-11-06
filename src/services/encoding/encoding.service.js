ef70dd7af65ee426335119627e20dc981f9ca3cd
Parkyunseo (PC yunseo-40) <pys33129747@ajou.ac.kr>
2025-11-06 23:12:09 +0900

    feat: s3 service 추가 및 encodingService와 s3Service 연동

--- src/services/encoding/encoding.service.js
+++ src/services/encoding/encoding.service.js
@@ -3,15 +3,41 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 import { ffmpegConfig } from "./ffmpeg.config.js";
+import path from 'path';
+import fs from 'fs';
 
 const execAsync = promisify(exec);
 
 export class EncodingService {
-    async transcodeVideo(inputPath, outputPath){
-        const command = `ffmpeg -i "${inputPath}" -c:v ${ffmpegConfig.videoCodec} -preset ${ffmpegConfig.preset} -crf${ffmpegConfig.crf} -y "$${outputPath}"`
+    constructor(s3Service){
+        this.s3Service = s3Service;
+    }
+
+    async transcodeVideo(inputPath, outputDir, filename){
+        if(!inputPath || !outputDir || !filename) {
+            throw new Error("inputPath, outputDir, filename이 없음");
+        }
+        
+        const outputPath = path.resolve(outputDir, filename);
+        const command = `ffmpeg -i "${inputPath}" -c:v ${ffmpegConfig.videoCodec} -preset ${ffmpegConfig.preset} -crf ${ffmpegConfig.crf} -y "${outputPath}"`;
+
         console.log(command);
-        await execAsync(command);
-        console.log(outputPath);
+
+        const{ stdout, stderr } = await execAsync(command);
+        console.log(stdout || stderr);
+
         return outputPath;
+    }
+
+    async s3Upload(filePath, s3Key){
+        if(!filePath || !s3Key) {
+            throw new Error("localFilePath와 s3Key가 없음");
+            throw new Error("filePath나 s3Key가 없습니다.");
+        }
+
+        const s3Url = await this.s3Service.uploadVideo(filePath, s3Key);
+        console.log(s3Url);
+
+        return s3Url;
     }
 }
---
+++ src/services/s3/s3.service.js
@@ -0,0 +1,51 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+} from "@aws-sdk/client-s3";
+import AWS from "aws-sdk";
+import fs from "fs";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export class S3Service {
+    constructor() {
+        this.s3 = new S3Client({
+            region: process.env.AWS_REGION,
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            },
+        });
+        this.bucketName = process.env.AWS_S3_BUCKET_NAME;
+    }
+
+    async uploadVideo(filePath, key) {
+        try {
+            if(!fs.existsSync(filePath)){
+                throw new Error("파일이 존재하지 않음");
+            }
+
+            const fileStream = fs.createReadStream(filePath);
+
+            const command = new PutObjectCommand({
+                Bucket: this.bucketName,
+                Key: key,
+                Body: fileStream,
+                ContentType: "video/mp4",
+            });
+            await this.s3.send(command);
+            
+            const fileUrl = `https://${this.bucketName}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
+            console.log(fileUrl);
+           
+            return fileUrl;
+        }
+        catch (err){
+            console.log(err.message);
+            throw err;
+        }
+    }
+}
