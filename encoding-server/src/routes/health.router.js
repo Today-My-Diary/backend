e9908b7822695166d603021fa88c396a14d78306
휘래 김 <hrkim2001@ajou.ac.kr>
2025-11-12 04:05:31 +0900

    Revert "[#%{source_issue_iid}] [Refactor] encoding server separation #5 (!today-my-diary/backend!4)"
    This reverts merge request !4

--- encoding-server/src/routes/health.router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { HealthController } from "../controllers/health.controller.js";
-
-const router = express.Router();
-const healthController = new HealthController();
-
-router.get("/", (req, res) => healthController.checkHealth(req, res));
-
-export default router;
