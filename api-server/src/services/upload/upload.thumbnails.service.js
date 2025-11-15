d14d99fa84f1b09bb9e0160803b2d7d237855c60
휘래 김 <hramst0618@gmail.com>
2025-11-15 18:56:14 +0900

    feat: thumbnail 저장 api 작성

--- api-server/src/services/upload/upload.thumbnails.service.js
+++ api-server/src/services/upload/upload.thumbnails.service.js
@@ -8,4 +8,17 @@ export class UploadThumbnailsService {
     getThumbnailUploadUrl = async (userId, uploadDate) => {
         return await this.s3Service.callGetPutObjectUrl(userId, uploadDate);
     };
+
+    completeThumbnailUpload = async (userId, uploadDate) => {
+        const s3Key = this.s3Service.generateThumbnailS3Key(userId, uploadDate);
+        const s3Url = await this.s3Service.getS3Url(s3Key);
+
+        // DB에 저장
+        const dataToUpsert = {
+            thumbnailS3Key: s3Key,
+            thumbnailS3Url: s3Url
+        };
+
+        await this.videoRepository.upsertByDate(userId, uploadDate, dataToUpsert);
+    }
 }
