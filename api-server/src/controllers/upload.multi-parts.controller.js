d8e70c52e43a72ecdbe23f922503c9ee330a7acc
휘래 김 <hramst0618@gmail.com>
2025-11-15 17:52:14 +0900

    refactor: S3 업로드 로직 분리 및 롤백 로직 추가

--- api-server/src/controllers/upload.multi-parts.controller.js
+++ api-server/src/controllers/upload.multi-parts.controller.js
@@ -1,14 +1,14 @@
 export class UploadMultiPartsController {
 
-    constructor(uploadBusiness) {
-        this.uploadBusiness = uploadBusiness;
+    constructor(uploadMultiPartsBusiness) {
+        this.uploadMultiPartsBusiness = uploadMultiPartsBusiness;
     }
 
     // POST /api/uploads/multi-parts/initiate
-    async initiateMultipartUpload(req, res, next) {
+    async initiateMultiPartsUpload(req, res, next) {
         try {
             const { uploadDate } = req.body;
-            const result = await this.uploadBusiness.initiateMultipartUpload(req.user.userId, uploadDate);
+            const result = await this.uploadMultiPartsBusiness.initiateMultiPartsUpload(req.user.userId, uploadDate);
             res.status(200).json(result);
         } catch (error) {
             next(error);
@@ -16,10 +16,10 @@ export class UploadMultiPartsController {
     }
 
     // POST /api/uploads/multi-parts/part
-    async getUploadPartUrl(req, res, next) {
+    async getUploadMultiPartsUrl(req, res, next) {
         try {
             const { uploadId, partNumber, uploadDate } = req.body;
-            const presignedUrl = await this.uploadBusiness.getUploadPartUrl(req.user.userId, uploadId, partNumber, uploadDate);
+            const presignedUrl = await this.uploadMultiPartsBusiness.getUploadMultiPartsUrl(req.user.userId, uploadId, partNumber, uploadDate);
             res.status(200).json({ presignedUrl });
         } catch (error) {
             next(error);
@@ -27,10 +27,10 @@ export class UploadMultiPartsController {
     }
 
     // POST /api/uploads/multi-parts/complete
-    async completeMultipartUpload(req, res, next) {
+    async completeMultiPartsUpload(req, res, next) {
         try {
             const { uploadId, parts, uploadDate, timestamps } = req.body;
-            const result = await this.uploadBusiness.completeMultipartUpload(req.user.userId, uploadId, parts, uploadDate, timestamps);
+            const result = await this.uploadMultiPartsBusiness.completeMultiPartsUpload(req.user.userId, uploadId, parts, uploadDate, timestamps);
             res.status(200).json(result);
         } catch (error) {
             next(error);
