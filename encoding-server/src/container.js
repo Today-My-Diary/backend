73a787fbe06b5f94a0e5c129d923ee4affbd7df6
휘래 김 <hramst0618@gmail.com>
2025-11-20 23:45:55 +0900

    [#6] Feat: HLS(ABR) 지원 및 .webm 확장자 인코딩 기능 추가 (!10)

--- encoding-server/src/container.js
+++ encoding-server/src/container.js
@@ -6,6 +6,7 @@ import { S3Client } from '@aws-sdk/client-s3';
 import { EncodingService } from './services/encoding/encoding.service.js';
 import { ffmpegConfig } from './services/encoding/ffmpeg.config.js';
 import { S3Service } from './services/s3/s3.service.js';
+import { ApiClient } from './services/api/api.client.js';
 
 // Business
 import { EncodingBusiness } from './business/encoding.business.js';
@@ -21,14 +22,18 @@ const s3Client = new S3Client({
 // 의존성 조립 (Bottom-Up)
 const s3Bucket = process.env.S3_BUCKET_NAME;
 const awsRegion = process.env.AWS_REGION;
+const apiServer = process.env.API_SERVER_URL;
+
 // Repositories
 
 // Services
 export const s3Service = new S3Service(s3Client, s3Bucket, awsRegion);
 const encodingService = new EncodingService(ffmpegConfig);
+export const apiClient = new ApiClient(apiServer);
 
 // Business
-const encodingBusiness = new EncodingBusiness(encodingService, s3Service);
+const encodingBusiness = new EncodingBusiness(encodingService, s3Service, apiClient);
+
 
 // Controllers
 export const encodingController = new EncodingController(encodingBusiness);
