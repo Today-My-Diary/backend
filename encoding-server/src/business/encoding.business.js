83d488e73007f265e1c45066bc07861053152138
휘래 김 <hramst0618@gmail.com>
2025-11-25 14:37:03 +0900

    fix: 변수명 오류를 수정 및 오류 처리 로직 스켈레톤 코드 작성

--- encoding-server/src/business/encoding.business.js
+++ encoding-server/src/business/encoding.business.js
@@ -5,35 +5,41 @@ export class EncodingBusiness {
         this.rabbitMQProducerService = rabbitMQProducerService;
     }
 
-    async handleEncoding ({userId, s3Key, s3Url}) {
-        const { workspace, jobId } = await this.encodingService.prepareWorkspace(userId);
-        const paths = this.encodingService.getHlsPaths(workspace, s3Key);
+    /**
+     * RabbitMQConsumer가 메시지를 받으면 호출하는 메서드
+     * @param {object} msgContent - { userId, s3Key, s3Url }
+     */
+    async handleEncoding ({ userId, s3Key, s3Url }) {
+        console.log(`[Encoding Start] User: ${userId}, File: ${s3Key}`);
 
         try{
-            console.log(`[Encoding Start] User: ${data.userId}, File: ${data.s3Key}`);
+            const { workspace, jobId } = await this.encodingService.prepareWorkspace(userId);
+            const paths = this.encodingService.getHlsPaths(workspace, s3Key);
 
             await this.encodingService.transcodeHls(s3Url, paths);
             await this.encodingService.generateMasterPlaylist(paths);
             const uploadResult = await this.s3Service.uploadAndBuildResponse({ workspace, userId, jobId });
 
+            // TODO: videoKey는 API 서버에 보낼 필요 없다면 생략 가능
             const { videoKey, hlsUrl } = uploadResult;
 
-            //     인코딩 결과
-            //              -> encodedS3Url: "https://s3.../encoded_video.m3u8"
-            //              -> status: 'COMPLETED'
-
-            // 가상의 결과 데이터 (재생 파일 위치) 예시입니다. (다음 항목들이 존재해야 합니다.)
+            // TODO: 응답 메시지 객체는 service에서 생성하도록 수정
             const resultMetadata = {
-                userId: data.userId,
-                originalS3Key: data.s3Key,
-                encodedS3Url: hlsUrl,  // 인코딩 결과
-                status: 'COMPLETED' // 인코딩 결과
+                userId: userId,
+                originalS3Key: s3Key,
+                encodedS3Url: hlsUrl,
+                status: 'COMPLETE'
             };
 
             await this.rabbitMQProducerService.sendMessage(resultMetadata);
             console.log(`[Encoding Finished] Result sent to API Server`);
 
+        } catch (error) {
+            console.error(`[Encoding Failed] User: ${userId}, File: ${s3Key}`, error);
+            // TODO: 인코딩 실패 시 API 서버에 'FAILED' 상태를 알려주는 로직 작성
+            //       예시 : this.rabbitMQProducerService.sendMessage({ ..., status: 'FAILED' });
         } finally {
+            console.log(`Cleaning up workspace: ${workspace}`);
             await this.encodingService.cleanupWorkspace(workspace);
         }
     }
