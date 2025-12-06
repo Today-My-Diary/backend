433560ae011066787fb7b979a4df905d2cb6666a
휘래 김 <hramst0618@gmail.com>
2025-12-06 14:49:50 +0900

    feat: error 처리 middleware 작성

--- api-server/src/middlewares/auth.middleware.js
+++ api-server/src/middlewares/auth.middleware.js
@@ -1,30 +1,35 @@
 import { tokenService, userService } from '../container.js';
+import {
+    InvalidTokenError,
+    TokenExpiredError,
+    UserNotFoundError
+} from '../errors/CustomError.js';
 
 export const authMiddleware = async (req, res, next) => {
     try {
         // 헤더에서 Access Token 추출
         const { authorization } = req.headers;
         if (!authorization) {
-            return res.status(401).json({ message: '인증 토큰이 없습니다.' });
+            throw new InvalidTokenError('인증 토큰이 없습니다.');
         }
 
         // "Bearer <token>" 형식 분리
         const [tokenType, token] = authorization.split(' ');
         if (tokenType !== 'Bearer' || !token) {
-            return res.status(401).json({ message: '유효하지 않은 토큰 형식입니다.' });
+            throw new InvalidTokenError('유효하지 않은 토큰 형식입니다.');
         }
 
         // Access Token 검증
         const payload = tokenService.verifyAccessToken(token);
 
         if (!payload) {
-            return res.status(401).json({ message: 'Access Token이 만료되었거나 유효하지 않습니다.' });
+            throw new TokenExpiredError('Access Token이 만료되었거나 유효하지 않습니다.');
         }
 
         const userId = payload.userId;
         const user = await userService.findUserById(userId);
         if (!user) {
-            return res.status(404).json({ message: '토큰 사용자를 찾을 수 없습니다.' });
+            throw new UserNotFoundError('토큰 사용자를 찾을 수 없습니다.');
         }
 
         req.user = user;
